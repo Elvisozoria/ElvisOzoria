e68c4b87140ee2e14a51fbe429adf4b9430aebe2
Elvis Ozoria <elvis.ozoria@gmail.com>
2016-02-12 22:39:40 -0800

    Cambio en contacto menu abajo
    ahora en vez de un formulario es un enlace a las redes sociales que uso

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-$("#btn-no").on("click",function() {alert("palomo");});
 
 
 $btnsi= $("#btn-si");
@@ -7,31 +6,10 @@ $preguntaCorreo= $("#preguntaCorreo");
 $enviarCorreo= $("#enviarCorreo");
 $contactoBtn= $("#contactoBtn");
 $contactoForm= $("#contactoForm");
+$contactoSocial= $("#contactoSocial");
 $contactoClose= $(".icon-close");
 $cargarMas= $("#cargarmas");
 
-$btnsi.on("click",function(e){
-
-	e.preventDefault();
-	console.log($formularioCorreo);
-	$formularioCorreo.removeClass("invisible");
-	$preguntaCorreo.addClass("invisible");
-});
-
-$cargarMas.on("click",function(e){
-	e.preventDefault();
-	$(".cargarmasContainer").append("<div class='nohaymas '><p>Ups, parece que no hay nada mas por aqui</p><p>Escribe aqui tu correo y seras el primero en saber cuando haya cosas nuevas aqui!</p><input type='mail' placeholder='Ej: holasoyjuan@hotmail.com'><a href='#' class='boton' id='primeroensaber'>Quiero ser el primero en saber cuando haya algo nuevo</a></div>");
-	$cargarMas.hide();
-	$(".cargarmasContainer img").hide();
-
-})
-
-$enviarCorreo.on('click',function(e){
-
-	//awesome shit should happen here
-	e.preventDefault();
-	alert("estas a punto de cambiar tu vida para mejor!")
-});
 
 $contactoBtn.on('click',contactoToggle);
 $contactoClose.on('click',contactoToggle);
@@ -46,6 +24,7 @@ function contactoToggle(e){
 		$('#contacto').toggleClass('temblar');
 		$('#contacto').toggleClass('fondoAzul');
 		$contactoForm.toggleClass('invisible')
+		// $contactoSocial.toggleClass('invisible')
 		$contactoBtn.fadeToggle();
 
 }
